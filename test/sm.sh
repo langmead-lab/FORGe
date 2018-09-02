c486e6822c574d7527109b7c4624f1bc1306372f
Ben Langmead <langmea@cs.jhu.edu>
2018-09-02 16:43:08 -0400

    add test script

---
+++ test/sm.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+INTERP=$1
+[ -z "${INTERP}" ] && echo "Must specify interpreter as arg" && exit 1
+shift
+
+set -ex
+
+echo "*** POP COV ***"
+${INTERP} src/rank.py \
+    --method popcov \
+    --reference test/sm.fa \
+    --vars test/sm.1ksnp \
+    --window-size 40 \
+    --prune 5 \
+    --output popcov.txt \
+    $*
+
+echo "*** HYBRID ***"
+${INTERP} src/rank.py \
+    --method hybrid \
+    --reference test/sm.fa \
+    --vars test/sm.1ksnp \
+    --window-size 40 \
+    --prune 5 \
+    --output hybrid.txt \
+    $*
